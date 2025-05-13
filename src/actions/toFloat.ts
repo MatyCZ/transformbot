60f7ac25a9c62e8a52f7ae47185b87e7dfeed11e
Matej Szendi <matej.szendi@bcom.cz>
2025-05-13 10:36:23 +0200

    * Fix action `toFloat()`

--- src/actions/toFloat.ts
+++ src/actions/toFloat.ts
@@ -1,16 +1,22 @@
-export function toFloat(input: string | number): number {
-  if (input === undefined || input === null) {
+import type { Action } from "../types.ts";
+
+export type ToFloatAction = Action<number | string, number>;
+
+export function toFloat(): ToFloatAction {
+  return (input) => {
+    if (input === undefined || input === null) {
+      return 0.0;
+    }
+
+    if (typeof input === "number") {
+      return isNaN(input) ? 0.0 : input;
+    }
+
+    if (typeof input === "string") {
+      const num = parseFloat(input.replace(",", "."));
+      return isNaN(num) ? 0.0 : num;
+    }
+
     return 0.0;
-  }
-
-  if (typeof input === "number") {
-    return isNaN(input) ? 0.0 : input;
-  }
-
-  if (typeof input === "string") {
-    const num = parseFloat(input.replace(",", "."));
-    return isNaN(num) ? 0.0 : num;
-  }
-
-  return 0.0;
+  };
 }
