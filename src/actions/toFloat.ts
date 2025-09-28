5c333fd7bd94cce389f574f2ec134ea083aaafd4
Matej Szendi <matej.szendi@bcom.cz>
2025-09-28 10:27:00 +0200

    * Refactor `float()` action to return its output as a string. Add support for parsing a trailing dot in the input string (e.g., when typing in an input field).

--- src/actions/toFloat.ts
+++ src/actions/toFloat.ts
@@ -1,31 +1,30 @@
 import type { Action } from "../types.ts";
 
-export type ToFloatAction = Action<number | string | null, number | null>;
+export type ToFloatAction = Action<string | null, string | null>;
 
-export function toFloat(): ToFloatAction {
+export function toFloat(fractionDigits?: number): ToFloatAction {
   return (input) => {
     if (input === null) {
       return null;
     }
 
-    if (typeof input === "number") {
-      if (isNaN(input)) {
-        return 0.0;
-      }
+    const normalized = input.replace(",", ".");
 
-      return input % 1 === 0 ? Number(input.toFixed(1)) : input;
+    // If the last character is a dot (e.g. writing decimal)
+    if ("." === normalized.trim().slice(-1)) {
+      return normalized;
     }
 
-    if (typeof input === "string") {
-      const num = parseFloat(input.replace(",", "."));
+    const num = parseFloat(normalized);
 
-      if (isNaN(num)) {
-        return 0.0;
-      }
-
-      return num % 1 === 0 ? Number(num.toFixed(1)) : num;
+    if (isNaN(num)) {
+      return null;
     }
 
-    return 0.0;
+    if (fractionDigits) {
+      return num.toFixed(fractionDigits);
+    }
+
+    return String(normalized);
   };
 }
